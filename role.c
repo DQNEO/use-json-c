f30b34257f926346f04c8141e65f6081690bbe67
DQNEO <dqneoo@gmail.com>
2014-11-10 00:56:15 +0900

    parse ec2 role

--- role.c
+++ role.c
@@ -5,16 +5,37 @@
  *  ./role  file.json
  */
 #include <stdio.h>
+#include <string.h>
 #include <json-c/json.h>
 
-void dump_json(struct json_object* const obj) {
-    json_object_object_foreach(obj, key, val) {
-        printf("%s: %s\n", key, json_object_to_json_string(val));
-    }
-}
+/**
+ * credentialsをまとめて持つ構造体
+ * iam roleで与えられるcredentialsはアクセスキーと
+ * シークレットキーのペア以外にもセキュリティトークンがある
+ */
+struct Credentials {
+    const char* accesskeyid;
+    const char* secretaccesskey;
+    const char* token;
+};
 
 int main(int argc, char **argv) {
-    struct json_object *obj2 = json_object_from_file(argv[1]);
-    dump_json(obj2);
+    struct json_object *obj = json_object_from_file(argv[1]);
+
+    struct Credentials crd;
+
+    json_object_object_foreach(obj, key, val) {
+        if (strcmp(key, "AccessKeyId") == 0) {
+            crd.accesskeyid = json_object_to_json_string(val);
+        } else if (strcmp(key, "SecretAccessKey") == 0) {
+            crd.secretaccesskey = json_object_to_json_string(val);
+        } else if (strcmp(key, "Token") == 0) {
+            crd.token = json_object_to_json_string(val);
+        }
+    }
+
+    printf("%s\n", crd.accesskeyid);
+    printf("%s\n", crd.secretaccesskey);
+    printf("%s\n", crd.token);
     return 0;
 }
