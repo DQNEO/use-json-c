8be2efd08075cbb3c375d47cce35643a4d6bdbb6
DQNEO <dqneoo@gmail.com>
2014-11-10 00:42:03 +0900

    example to parse ec2role

---
+++ role.c
@@ -0,0 +1,21 @@
+/**
+ * Sample for using json-c
+ * gcc -I /opt/json-c/include -L /opt/json-c/lib -ljson-c json.c  -o json
+ *
+ * Usage:
+ *  ./json sample.json
+ */
+#include <stdio.h>
+#include <json-c/json.h>
+
+void dump_json(struct json_object* const obj) {
+    json_object_object_foreach(obj, key, val) {
+        printf("-- \t%s: %s\n", key, json_object_to_json_string(val));
+    }
+}
+
+int main(int argc, char **argv) {
+    struct json_object *obj2 = json_object_from_file(argv[1]);
+    dump_json(obj2);
+    return 0;
+}
